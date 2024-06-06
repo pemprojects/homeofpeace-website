caf43773143ba5c86e6b848f16fb70178b50c121
Peter Mbanda <pmbandaft@gmail.com>
2024-06-05 19:03:18 -0500

    Updates

---
+++ precache-manifest.cd8fc2e72ad2826c6ef7cf753f8daf05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9b94e08f820a90064819144f98a9e0",
+    "url": "/homeofpeace-website/index.html"
+  },
+  {
+    "revision": "c9887eaafc329a97f65e",
+    "url": "/homeofpeace-website/static/css/main.5c8fc7a9.chunk.css"
+  },
+  {
+    "revision": "34c96fa74201388e3da5",
+    "url": "/homeofpeace-website/static/js/2.62239a1c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/homeofpeace-website/static/js/2.62239a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9887eaafc329a97f65e",
+    "url": "/homeofpeace-website/static/js/main.29ab1576.chunk.js"
+  },
+  {
+    "revision": "535d7105bc061815d240",
+    "url": "/homeofpeace-website/static/js/runtime-main.41293cd8.js"
+  },
+  {
+    "revision": "60ab3e09fa881846bb0428bfe73b3416",
+    "url": "/homeofpeace-website/static/media/church_service.60ab3e09.jpg"
+  },
+  {
+    "revision": "245986ff9c1ab53011533aafb89af219",
+    "url": "/homeofpeace-website/static/media/hope_buildings.245986ff.jpg"
+  },
+  {
+    "revision": "bb8b852fe9f3a199b8e7200407e80230",
+    "url": "/homeofpeace-website/static/media/hope_church_cert.bb8b852f.jpg"
+  }
+]);
